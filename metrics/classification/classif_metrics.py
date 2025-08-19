4d4706986a091d744b46db9ba223a37da2afb3d8
Mikkel-AI0700 <mikeruai0700@gmail.com>
2025-08-19 17:08:15 +0800

    Added helper functions to centralize the prediction countings and validations

--- metrics/classification/classif_metrics.py
+++ metrics/classification/classif_metrics.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, Callable
 import numpy as np
 import pandas as pd
 from validator.validator import DatasetValidation, ClassificationMetricValidation
@@ -6,11 +6,77 @@ from validator.validator import DatasetValidation, ClassificationMetricValidatio
 dset_val = DatasetValidation()
 classif_val = ClassificationMetricValidation()
 
-def accuracy (true_y: Union[np.ndarray | pd.DataFrame], pred_y: Union[np.ndarray | pd.DataFrame]):
-    pass
+"""
+TODO:
+    -- Write logic for binary classification
+        --- Write comparing logic for TP, TN, FP, FN
+    -- Write logic for multiclass classification
+        --- Write comparing logic for multiclass TP, TN, FP, FN
+
+REMOVE IN DEPLOYMENT!
+"""
+def metric_preds_counter (
+    true_y: np.ndarray,
+    pred_y: np.ndarray,
+    prediction_type: str = "binary"
+):
+    if prediction_type == "binary":
+        true_positive = np.sum(np.logical_and(true_y == 1, pred_y == 1))
+        true_negative = np.sum(np.logical_and(true_y == 0, pred_y == 0))
+        false_positive = np.sum(np.logical_and(true_y == 0, pred_y == 1))
+        false_negative = np.sum(np.logical_and(true_y == 1, pred_y == 0))
+
+        return (
+            true_positive,
+            true_negative,
+            false_positive,
+            false_negative
+        )
+
+    if prediction_type == "multiclass":
+        pass
+
+def metric_validator_helper (
+    true_y: np.ndarray,
+    pred_y: np.ndarray, 
+    classif_metric: Callable
+):
+    dataset_validated = False
+    classif_metric_validated = False
+
+    if (dset_val.validate_existence([true_y, pred_y]) and
+        dset_val.validate_shapes(true_y, pred_y) and
+        dset_val.validate_types(true_y, pred_y)
+    ):
+        dataset_validated = True
+
+    if (classif_val.validate_zero_division(true_y) and
+        classif_val.validate_label_count(true_y, classif_metric)
+    ):
+        classif_metric_validated = True
+
+    if dataset_validated and classif_metric_validated:
+        return True
+
+def accuracy (
+    true_y: Union[np.ndarray | pd.DataFrame], 
+    pred_y: Union[np.ndarray | pd.DataFrame],
+    average="binary"
+):
+    if metric_validator_helper(true_y, pred_y, accuracy):
+        # Summing up the total count of TP and TN using Numpy's logical_and() function
+        true_positive = np.sum(np.logical_and(true_y == 1, pred_y == 1))
+        true_negative = np.sum(np.logical_and(true_y == 0, pred_y == 0))
+
+        return (true_positive + true_negative) / len(pred_y)
 
 def precision (true_y: Union[np.ndarray | pd.DataFrame], pred_y: Union[np.ndarray | pd.DataFrame]):
-    pass
+    if metric_validator_helper(true_y, pred_y, accuracy):
+        # Summing up the total count of TP and FP using Numpy's logical_and() function
+        true_positive = np.sum(np.logical_and(true_y == 1, pred_y == 1))
+        false_positive = np.sum(np.logical_and(true_y == 0, pred_y == 1))
+
+        return true_positive / (true_positive + false_positive)
 
 def recall (true_y: Union[np.ndarray | pd.DataFrame], pred_y: Union[np.ndarray | pd.DataFrame]):
     pass
