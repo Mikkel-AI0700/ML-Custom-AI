82d4fefaf7cb6979611bbaaac9cf5397e0775a9b
Mikkel-AI0700 <mikeruai0700@gmail.com>
2025-08-23 07:21:21 +0800

    Updated gradient computation and update logic and added validation inside fit method

--- logistic-regression/source-files/log-reg-copy.py
+++ logistic-regression/source-files/log-reg-copy.py
@@ -2,7 +2,7 @@ from typing import Union
 import numpy as np
 import pandas as pd
 import scipy as scp
-from validator.validator import Validate
+from validator.validator import DatasetValidation
 from metrics.classification.classif_metrics import log_loss
 
 class LogisticRegression:
@@ -27,28 +27,37 @@ class LogisticRegression:
         else:
             self.partial_derivative_b = 0.0
 
-    def _compute_weights_derivative (self, train_x: np.ndarray, train_y: np.ndarray,pred_y: np.ndarray):
-        return -2 / len(train_x) * np.sum(train_x * (train_y - pred_y))
+    def _compute_weights_derivative (self, train_x: np.ndarray, train_y: np.ndarray, pred_y: np.ndarray):
+        return 1 / len(train_x) * train_x.T * (pred_y - train_y)
 
     def _compute_bias_derivative (self, train_x: np.ndarray, train_y: np.ndarray, pred_y: np.ndarray):
-        return -2 / len(train_x) * np.sum(train_y - pred_y)
+        return 1 / len(train_x) * np.sum(pred_y - train_y)
 
     def _update_weights_derivatives (self, computed_weights_gradients: np.ndarray):
-        self.partial_derivative_m = self.learning_rate - computed_weights_gradients
+        self.partial_derivative_m = self.partial_derivative_m - self.learning_rate * computed_weights_gradients
 
     def _update_bias_derivatives (self, computed_bias_gradients: np.ndarray):
-        self.partial_derivative_b = self.learning_rate - computed_bias_gradients
+        self.partial_derivative_b = self.partial_derivative_m - self.learning_rate * computed_bias_gradients
 
     def _sigmoid_function (self, pred_y: np.ndarray):
         return 1 / 1 + np.e ** -pred_y
 
+    def _cost_function (self, computed_loss: np.ndarray):
+        return -1 / len(computed_loss) * np.sum(computed_loss)
+
     def fit (self, train_x: Union[np.ndarray | pd.DataFrame], train_y: Union[np.ndarray | pd.DataFrame]):
+        if (self.validator.validate_existence([train_x, train_y]) and
+            self.validator.validate_shapes(train_x, train_y)
+        ):
+            pass
+
         for epoch in range(self.epochs):
             print(f"Epoch: {epoch} | M: {self.partial_derivative_m} | B: {self.partial_derivative_b}")
 
             # Training inference and loss calculating
             predictions = np.dot(train_x, self.partial_derivative_m)
-
+            sigmoid_probabilities = self._sigmoid_function(predictions)
+            computed_cost = log_loss(train_y, sigmoid_probabilities)
 
             # Gradient computation and updating
             computed_weights = self._compute_weights_derivative(train_x, train_y, predictions)
