86822290a1d0deeb28f1904be3144befd7d059f6
Mikkel-AI0700 <mikeruai0700@gmail.com>
2025-08-27 22:30:42 +0800

    Fixed the entire function logic of the main function

--- run-script.sh
+++ run-script.sh
@@ -2,13 +2,16 @@
 
 # Global constants
 PYTHON_TLD="$(pwd)"
-LIN_REG_SOURCE = "/home/mikkel/Desktop/ai-projects/machine-learning/custom-ai/linear-regression/source-file/lin-reg-source.py"
-LOG_REG_SOURCE = "/home/mikkel/Desktop/ai-projects/machine-learning/custom-ai/logistic-regression/source-files/log-reg-main.py"
+LIN_REG_SOURCE="/home/mikkel/Desktop/ai-projects/machine-learning/custom-ai/linear-regression/source-file/lin-reg-source.py"
+LOG_REG_SOURCE="/home/mikkel/Desktop/ai-projects/machine-learning/custom-ai/logistic-regression/source-files/log-reg-main.py"
+
+tld_passed=false
+venv_passed=false
 
 function check_tld () {
-    if [[ $PYTHONPATH == ${PYTHON_TLD} ]] ; then
+    if [[ "${PYTHONPATH}" == "${PYTHON_TLD}" ]] ; then
         echo "[+] Python TLD is set"
-        echo "true"
+        tld_passed=true
     else
         echo "[-] Error: Python TLD is not set. Setting now"
         export PYTHONPATH=${PYTHON_TLD}
@@ -16,9 +19,9 @@ function check_tld () {
 }
 
 function check_venv () {
-    if [[ -n $VIRTUAL_ENV ]] ; then
+    if [[ -n "$VIRTUAL_ENV" ]] ; then
         echo "[+] Python virtual environment is set"
-        echo "true"
+        venv_passed=true
     else
         echo "[-] Error: Python virtual environment isn't set. Setting now"
         source main-venv/bin/activate
@@ -26,17 +29,14 @@ function check_venv () {
 }
 
 function main () {
-    if [[ ${check_tld} == "true"]] && [[ ${check_venv} == "true" ]] ; then
-        if [[ $1 == "lin" ]] || [[ $1 == "lin-reg" ]] ; then
-            python3 ${LIN_REG_SOURCE}
-        else if [[ $1 == "log" ]] || [[ $1 == "log-reg" ]] ; then
-            python3 ${LOG_REG_SOURCE}
-        else
-            echo -e "[-] Error: User passed argument doesn't exist"
-            exit 1
-        fi
-    fi
+  if [[ ${tld_passed} == true && ${venv_passed} == true ]] ; then
+      if [[ "$1" == "linreg" ]] ; then
+          python3 ${LIN_REG_SOURCE}
+      elif [[ "$1" == "logreg" ]] ; then
+          :
+      fi
+  fi
 }
 
-main
+main $1
 
