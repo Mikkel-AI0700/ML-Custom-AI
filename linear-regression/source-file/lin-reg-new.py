576d32db2a9b96bdd18dafff9cf11835614ae744
Mikkel-AI0700 <mikeruai0700@gmail.com>
2025-09-01 15:15:14 +0800

    Fixed and updated several logical errors in the init, initialize_weights_bias, and fit method

--- linear-regression/source-file/lin-reg-new.py
+++ linear-regression/source-file/lin-reg-new.py
@@ -1,7 +1,12 @@
 from typing import Union
 import numpy as np
 import pandas as pd
-from validator.validators import DatasetValidation
+from validator.validator import DatasetValidation
+
+# Importing for testing purposes
+# Will be removed in the lin-reg-source.py
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import make_regression
 
 class LinearRegression:
     def __init__ (self, epoch: int, learning_rate: float = 1e-4, fit_intercept: bool = True):
@@ -9,11 +14,16 @@ class LinearRegression:
         self.partial_derivative_b = None
         self.epochs = epoch
         self.learning_rate = learning_rate
-        self.fit_intercept = True
-        self.validator = DatasetValidator()
+        self.fit_intercept = fit_intercept
+        self.validator = DatasetValidation()
 
-    def _initialize_weights_bias (self):
-        pass
+    def _initialize_weights_bias (self, train_x: np.ndarray):
+        self.partial_derivative_m = np.zeros((train_x.shape[1]))
+
+        if self.fit_intercept:
+            self.partial_derivative_b = np.hstack([np.ones((train_x.shape[0], 1)), train_x])
+        else:
+            self.partial_derivative_b = 0.0
 
     def _compute_weights_gradient (self, train_x: np.ndarray, train_y: np.ndarray, pred_y: np.ndarray):
         return -(2 / len(train_x)) * np.sum(train_x * (train_y * pred_y))
@@ -28,6 +38,7 @@ class LinearRegression:
         self.partial_derivative_b -= self.learning_rate * computed_bias_gradients
 
     def fit (
+        self,
         train_x: Union[np.ndarray | pd.DataFrame],
         train_y: Union[np.ndarray | pd.DataFrame],
     ):
@@ -38,8 +49,12 @@ class LinearRegression:
 
         if isinstance(train_x, pd.DataFrame):
             train_x = train_x.to_numpy()
+            print(f"[+] Train x is Pandas. Converted to Numpy -> Rows: {train_x.shape[0]} | Columns: {train_x.shape[1]}")
         if isinstance(train_y, pd.DataFrame):
             train_y = train_y.to_numpy()
+            print(f"[+] Train y is Pandas. Converted to Numpy -> Rows: {train_y.shape[0]} | Columns: {train_y.shape[1]}")
+
+        self._initialize_weights_bias(train_x)
 
         for epoch in range(self.epochs):
             print(f"Batch: {epoch + 1} | M: {self.partial_derivative_m} | B: {self.partial_derivative_b}")
@@ -59,7 +74,26 @@ class LinearRegression:
 
         return np.dot(test_x, self.partial_derivative_m)
 
-
+# TESTING PURPOSES ONLY!
+# main() FUNCTION WILL BE REMOVED IN lin-reg-source.py
 def main ():
-    pass
+    linreg_instance = LinearRegression(epoch=35)
+    X, Y = make_regression(
+        n_samples=1000,
+        n_features=5,
+        random_state=42
+    )
+
+    tr_x, ts_x, tr_y, ts_y = train_test_split(
+        X,
+        Y,
+        train_size=0.7,
+        test_size=0.3,
+        shuffle=True,
+        random_state=42
+    )
+
+    linreg_instance.fit(tr_x, tr_y)
+
+main()
 
