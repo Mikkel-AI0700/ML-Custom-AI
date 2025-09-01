85b519370b54a2a319a0158538c518dbbd572227
Mikkel-AI0700 <mikeruai0700@gmail.com>
2025-09-01 15:24:37 +0800

    Fixed multiple variable assignment and control flow issues

--- run-script.sh
+++ run-script.sh
@@ -1,36 +1,43 @@
 #!/bin/bash
 
 # Global constants
-PYTHON_TLD="$(pwd)"
-LIN_REG_SOURCE="/home/mikkel/Desktop/ai-projects/machine-learning/custom-ai/linear-regression/source-file/lin-reg-source.py"
+PYTHON_TLD=$(pwd)
+LIN_REG_SOURCE="/home/mikkel/Desktop/ai-projects/machine-learning/custom-ai/linear-regression/source-file/lin-reg-new.py"
 LOG_REG_SOURCE="/home/mikkel/Desktop/ai-projects/machine-learning/custom-ai/logistic-regression/source-files/log-reg-main.py"
 
-tld_passed=false
-venv_passed=false
+tld_passed=0
+venv_passed=0
 
 function check_tld () {
     if [[ "${PYTHONPATH}" == "${PYTHON_TLD}" ]] ; then
         echo "[+] Python TLD is set"
-        tld_passed=true
+        tld_passed=1
     else
         echo "[-] Error: Python TLD is not set. Setting now"
-        export PYTHONPATH=${PYTHON_TLD}
+        export PYTHONPATH=${PYTHON_TLD} && echo "[+] Python TLD set: ${PYTHON_TLD}"
+        check_tld
     fi
 }
 
 function check_venv () {
     if [[ -n "$VIRTUAL_ENV" ]] ; then
         echo "[+] Python virtual environment is set"
-        venv_passed=true
+        venv_passed=1
     else
         echo "[-] Error: Python virtual environment isn't set. Setting now"
-        source main-venv/bin/activate
+        source main-venv/bin/activate && echo "[+] Environment set: ${VIRTUAL_ENV}"
+        check_venv
     fi
 }
 
 function main () {
-  if [[ ${tld_passed} == true && ${venv_passed} == true ]] ; then
-      if [[ "$1" == "linreg" ]] ; then
+  check_tld
+  check_venv
+
+  if [[ ${tld_passed} -eq 1 && ${venv_passed} -eq 1 ]] ; then
+      echo "[+] TLD and VENV set. Running: $1"
+      if [[ $1 == "linreg" ]] ; then
+          echo "[+] Running Linear Regression model"
           python3 ${LIN_REG_SOURCE}
       elif [[ "$1" == "logreg" ]] ; then
           :
