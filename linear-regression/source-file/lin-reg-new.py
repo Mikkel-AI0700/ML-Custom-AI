d7be95bde3dba150dda633019127bf4338f9d611
Mikkel-AI0700 <mikeruai0700@gmail.com>
2025-09-04 20:44:24 +0800

    Added print statements and made a slight change to the gradient descent formula

--- linear-regression/source-file/lin-reg-new.py
+++ linear-regression/source-file/lin-reg-new.py
@@ -7,7 +7,8 @@ from validator.validator import DatasetValidation
 # Will be removed in the lin-reg-source.py
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import make_regression
-
+from sklearn.metrics import mean_squared_error
+ 
 class LinearRegression:
     def __init__ (self, epoch: int, learning_rate: float = 1e-4, fit_intercept: bool = True):
         self.partial_derivative_m = None
@@ -26,7 +27,7 @@ class LinearRegression:
             self.partial_derivative_b = 0.0
 
     def _compute_weights_gradient (self, train_x: np.ndarray, train_y: np.ndarray, pred_y: np.ndarray):
-        return -(2 / len(train_x)) * np.sum(train_x * (train_y * pred_y))
+        return -(2 / len(train_x)) * np.dot(train_x.T, (train_y - pred_y))
 
     def _compute_bias_gradients (self, train_y: np.ndarray, pred_y: np.ndarray):
         return -(2 / len(train_y)) * np.sum(train_y - pred_y)
@@ -45,7 +46,7 @@ class LinearRegression:
         if (self.validator.validate_existence([train_x, train_y]) and
             self.validator.validate_shapes(train_x, train_y)
         ):
-            pass
+            print("Validator checks passed. Proceeding with dataset checking")
 
         if isinstance(train_x, pd.DataFrame):
             train_x = train_x.to_numpy()
@@ -57,7 +58,7 @@ class LinearRegression:
         self._initialize_weights_bias(train_x)
 
         for epoch in range(self.epochs):
-            print(f"Batch: {epoch + 1} | M: {self.partial_derivative_m} | B: {self.partial_derivative_b}")
+            print(f"Batch: {epoch + 1} | M: {self.partial_derivative_m}")
             
             # Main training loop
             predictions = np.dot(train_x, self.partial_derivative_m)
@@ -70,14 +71,17 @@ class LinearRegression:
         if self.validator.validate_existence([test_x]):
             pass
         if isinstance(test_x, pd.DataFrame):
+            print("Test x is Pandas. Converted to Numpy -> Rows: {train_x.shape[0]} | Columns: {train_x.shape[1]}")
             test_x.to_numpy()
+
+        #test_x = np.hstack([np.ones((test_x.shape[0], 1)), test_x])
 
         return np.dot(test_x, self.partial_derivative_m)
 
 # TESTING PURPOSES ONLY!
 # main() FUNCTION WILL BE REMOVED IN lin-reg-source.py
 def main ():
-    linreg_instance = LinearRegression(epoch=35)
+    linreg_instance = LinearRegression(epoch=35, learning_rate=1e-4)
     X, Y = make_regression(
         n_samples=1000,
         n_features=5,
@@ -94,6 +98,9 @@ def main ():
     )
 
     linreg_instance.fit(tr_x, tr_y)
+
+    preds = linreg_instance.predict(ts_x)
+    print(f"[+] MSE: {mean_squared_error(ts_y, preds)}")    
 
 main()
 
