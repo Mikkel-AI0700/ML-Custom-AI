d775a37722ac7f6c49b5f906334b88130880184e
Mikkel-AI0700 <mikeruai0700@gmail.com>
2025-09-22 20:27:26 +0800

    Created ParameterErrors for exception handling for parameter validation

---
+++ errors/ParameterErrors.py
@@ -0,0 +1,12 @@
+class OutOfBoundException (ValueError):
+    def __init__ (self, error_message):
+        pass
+
+class InvalidSelectionException (ValueError):
+    def __init__ (self, error_message)
+        pass
+
+class NonExistentHyperparameterException (ValueError):
+    def __init__ (self):
+        pass
+
