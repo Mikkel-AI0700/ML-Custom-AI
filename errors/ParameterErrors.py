2a473394676ec9d5d441998b0d4151595a995f55
Mikkel-AI0700 <mikeruai0700@gmail.com>
2025-09-22 21:16:29 +0800

    Added __str__ dunder methods to return the raised exception message on try-except invokes

--- errors/ParameterErrors.py
+++ errors/ParameterErrors.py
@@ -1,12 +1,21 @@
-class OutOfBoundException (ValueError):
+class OutOfBoundsException (ValueError):
     def __init__ (self, error_message):
-        pass
+        self.passed_oob_message = error_message
+
+    def __str__ (self):
+        return self.passed_oob_message
 
 class InvalidSelectionException (ValueError):
     def __init__ (self, error_message)
-        pass
+        self.passed_ie_message = error_message
+
+    def __str__ (self):
+        return self.passed_ie_message
 
 class NonExistentHyperparameterException (ValueError):
-    def __init__ (self):
-        pass
+    def __init__ (self, error_message):
+        self.passed_neh_message = error_message
+
+    def __str__ (self):
+        return self.passed_neh_message
 
