df1ddf43dc9145a8c957999a1f261eb29e96bb7c
Mikkel-AI0700 <mikeruai0700@gmail.com>
2026-01-08 00:15:21 +0800

    Added specific parent classes for the machine learning algorithms to inherit from

---
+++ base/EstimatorClass.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+from validator.DatasetValidation import DatasetValidation
+
+class BaseEstimator:
+    pass
+
+class RegressorMixin:
+    pass
+
+class ClassifierMixin:
+    pass
+
