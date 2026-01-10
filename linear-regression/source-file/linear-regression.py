4525f4bbd98b011d6a121de2e8e40c05edb5a88d
Mikkel-AI0700 <mikeruai0700@gmail.com>
2026-01-10 11:47:08 +0800

    Added documentation to everything

--- linear-regression/source-file/linear-regression.py
+++ linear-regression/source-file/linear-regression.py
@@ -10,6 +10,18 @@ from sklearn.datasets import make_regression
 from sklearn.metrics import mean_squared_error
  
 class LinearRegression:
+    """
+    Supervised machine learning model that models the relationship of the
+    independent and dependent variables
+
+    Parameters:
+        epochs (int): The amount of iterations that the dataset will go through the model
+        learning_rate (Union[int | float): Controls how much the gradients will be updated. Controls gradient step
+        fit_intercept (bool): Controls whether to include the bias. If no, then model will assume data goes through (0, 0) 
+        
+    Returns:
+        LinearRegression (object): The instantiated LinearRegression object
+    """
     def __init__ (self, epoch: int, learning_rate: float = 1e-4, fit_intercept: bool = True):
         self.partial_derivative_m = None
         self.partial_derivative_b = None
@@ -19,24 +31,82 @@ class LinearRegression:
         self.validator = DatasetValidation()
 
     def _initialize_weights_bias (self, train_x: np.ndarray):
+        """
+        Initializes the weights and bias of the linear regression model
+
+        Parameters: 
+            train_x (np.ndarray): The main training dataset to be used for training
+
+        Returns:
+            None
+        """
         self.partial_derivative_m = np.zeros((train_x.shape[1]))
 
         if self.fit_intercept:
             self.partial_derivative_b = 0.0
 
     def _compute_cost (self, train_y: np.ndarray, pred_y: np.ndarray):
+        """
+        Computing the Mean Squared Error (MSE) error of the linear regression model
+
+        Parameters:
+            train_y (np.ndarray): The training ground truths of the training dataset
+            pred_y (np.ndarray): The initial predictions of the model during training
+
+        Returns:
+            computed_mse (np.float32): The computed MSE error for the model
+        """
         return np.sum(np.square(pred_y - train_y))
 
     def _compute_weights_gradient (self, train_x: np.ndarray, train_y: np.ndarray, pred_y: np.ndarray):
+        """
+        Computes the required gradients to adjust model's weights using: dw = (1 / m) * sum((y_pred - y) * x)
+
+        Parameters:
+            train_x (np.ndarray): The main training dataset
+            train_y (np.ndarray: The ground truths dataset
+            pred_y (np.ndarray): The ndarray of logits after Xw + b
+
+        Returns:
+            computed_weights_gradients (np.ndarray): A ndarray of computed gradients to update model weights
+        """
         return 1 / len(train_x) * np.dot(train_x.T, (pred_y - train_y))
 
     def _compute_bias_gradients (self, train_y: np.ndarray, pred_y: np.ndarray):
+        """
+        Computes the required bias to adjust the model's bias using: db = (1 / m) * sum(y_pred - y)
+
+        Parameters:
+            train_y (np.ndarray): The ground truths dataset
+            pred_y (np.ndarray): The ndarray of logits after Xw + b
+
+        Returns:
+            computed_bias_gradient (np.float32): A np.float32 of computed bias to update the model's bias
+        """
         return 1 / len(train_y) * np.sum(pred_y - train_y)
 
     def _update_weights_gradient (self, computed_weights_gradients: np.ndarray):
+        """
+        Updates the model's weights using: w = w - (learning_rate * dw)
+
+        Parameters:
+            computed_weights_gradients (np.ndarray): A ndarray containing the computed gradients from _compute_weights_gradients
+
+        Returns:
+            None
+        """
         self.partial_derivative_m -= self.learning_rate * computed_weights_gradients
 
     def _update_bias_gradient (self, computed_bias_gradients: np.ndarray):
+        """
+        Updates the model's bias using: b = b - (learning_rate * db)
+
+        Parameters:
+            computed_bias_gradient (float): A single float value containing the computed bias from _compute_bias_gradients
+
+        Returns:
+            None
+        """
         self.partial_derivative_b -= self.learning_rate * computed_bias_gradients
 
     def fit (
@@ -44,6 +114,16 @@ class LinearRegression:
         train_x: Union[np.ndarray | pd.DataFrame],
         train_y: Union[np.ndarray | pd.DataFrame],
     ):
+        """
+        Starts training the logistic regression model
+
+        Parameters:
+            train_x: (Union[np.ndarray | pd.DataFrame]): The main training dataset
+            train_y: (Union[np.ndarray | pd.DataFrame]): The main ground truths dataset
+
+        Returns:
+            None
+        """
         if (self.validator.validate_existence([train_x, train_y]) and
             self.validator.validate_shapes(train_x, train_y)
         ):
@@ -68,6 +148,15 @@ class LinearRegression:
             self._update_bias_gradient(computed_bias) 
 
     def predict (self, test_x: Union[np.ndarray | pd.DataFrame]):
+        """
+        Taking in a np.ndarray of test samples then inferencing them using: Xw + b
+
+        Parameters:
+            test_x (Union[np.ndarray | pd.DataFrame]): The dataset that will be inferenced
+
+        Returns:
+            predictions (np.ndarray): The inferenced elements
+        """
         if self.validator.validate_existence([test_x]):
             pass
         if isinstance(test_x, pd.DataFrame):
