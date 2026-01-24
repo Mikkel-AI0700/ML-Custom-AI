1848de59e5c750ab8dca0de3b3b39677d146dca5
Mikkel-AI0700 <mikeruai0700@gmail.com>
2026-01-24 11:03:11 +0800

    Fixed the validation and syntax format of the script and added logic that will run specifc machine learning algorithms using hashmaps and for-loops

--- run-script.sh
+++ run-script.sh
@@ -10,7 +10,7 @@ DATASET_GENERATOR_PATH="${PYTHON_TLD}/python-utilities/generator-files/generator
 # Machine learning algorithms path
 LIN_REG_SOURCE="${PYTHON_TLD}/linear-regression/source-file/lin-reg-new.py"
 LOG_REG_SOURCE="${PYTHON_TLD}/logistic-regression/source-files/log-reg-new.py"
-TREE_SOURCE=""
+TREE_SOURCE="${PYTHON_TLD}/decision-tree/source-code/tree.py"
 
 # Global Top Level Domain and VENV checks
 main_tld_passed=0
@@ -20,44 +20,82 @@ function check_tld_venv () {
     if [[ ${main_tld_passed} -eq 1 && ${main_venv_passed} -eq 1 ]]; then
         echo "[+] PYTHON_TLD and VIRTUAL ENVIRONMENT IS SET"
     else
-        { export PYTHON_TLD="${PYTHON_TLD}" && echo "[+] PYTHON_TLD is now set!" && main_tld_passed=1; } || { echo "[-] Unable to set PYTHON_TLD" && exit 1; }
-        { source "main-venv/bin/activate" && echo "[+] VENV is now set!" && main_venv_passed=1; } || { echo "[-] Unable to set VENV" && exit 1; }
         check_tld_venv
+
+        # Set the PYTHON_TLD
+        {
+            export PYTHON_TLD="${PYTHON_TLD}" &&
+            echo "[+] PYTHON_TLD is now set!" &&
+            main_tld_passed=1;
+        } || {
+            echo "[-] Unable to set PYTHON_TLD" &&
+            exit 1;
+        }
+
+        # Set the Python virtual environment
+        {
+            source "main-venv/bin/activate" && 
+            echo "[+] VENV is now set!" &&
+            main_venv_passed=1;
+        } || {
+            echo "[-] Unable to set VENV" &&
+            exit 1;
+        }
     fi
 }
 
 function generate_datasets () {
     local algorithm_type="$1"
     local key_value_change="$2"
+    local valid_algorithms=("regression" "classification" "clustering")
 
-    if [[ ${gen_tld_passed} -eq 1 && ${gen_venv_passed} -eq 1 ]]; then
-        if [[ "${algorithm_type}" == "regression" ]]; then
-            :
-        elif [[ "${algorithm_type}" == "classification" ]]; then
-            :
-        elif [[ "${algorithm_type}" == "clustering" ]]; then
-            :
-        else
-            :
-        fi
+    if [[ ${main_tld_passed} -eq 1 && ${main_venv_passed} -eq 1 ]]; then
+        for algorithm in "${valid_algorithms[@]}"; do
+            if [[ ! "${algorithm_type}" != "${algorithm}" ]]; then
+                echo "[-] Error: Invalid algorithm type passed"
+                exit 1
+            fi
+        done
+
+        python3 "${DATASET_GENERATOR_PATH}" \
+            --dataset-type "${algorithm_type}" \
+            --key-value-change "${key_value_change}"
     fi
+}
+
+function execute_model () {
+    local algorithm_to_use="$1"
+    declare -A machine_learning_algorithms=(
+        ["linreg"]="${LIN_REG_SOURCE}"
+        ["logreg"]="${LOG_REG_SOURCE}"
+        ["tree"]="${TREE_SOURCE}"
+    )
+
+    for algorithm in "${!machine_learning_algorithms[@]}"; do
+        algorithm_source="${machine_learning_algorithms[${algorithm}]}"
+        if [[ "${algorithm}" == "${algorithm_to_use}" ]]; then
+            echo "[+] Running: ${algorithm_source}"
+            python3 "${algorithm_source}"
+        fi
+    done
 }
 
 function main () {
     check_tld_venv
+    local mode="$1"
+    local algorithm_type="$2"
+    local dataset_type="$3"
+    local key_value_change="$4"
 
-    if [[ ${tld_passed} -eq 1 && ${venv_passed} -eq 1 ]] ; then
-        echo "[+] TLD and VENV set. Running: $1"
-        if [[ $1 == "linreg" ]] ; then
-            echo "[+] Running Linear Regression model"
-            python3 ${LIN_REG_SOURCE}
-        elif [[ "$1" == "logreg" ]] ; then
-            python3 ${LOG_REG_SOURCE}
-        elif [[ "$1" == "tree" ]]; then
-            :
-        fi
+    if [[ "${mode}" == "dataset" ]]; then
+        generate_datasets "${dataset_type}" "${key_value_change}"
+    elif [[ "${mode}" == "algorithm" ]]; then
+        execute_model "${algorithm_type}"
+    else
+        echo "[-] Unrecognized option"
+        exit 1
     fi
 }
 
-main $1
+main $@
 
